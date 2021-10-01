97f173ecfe56275aefd3094adfec267c53e1164d
zubry <zubryjs@gmail.com>
2021-10-01 17:19:15 -0400

    Enhance Vote component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Vote from './components/Vote'
 function App() {
   return (
     <List>
-      <Vote name="James" value="0.5" />
+      <Vote name="James" value="0.5" isRevealed/>
+      <Vote name="James 1" isRevealed />
+      <Vote name="James 1" />
       <Vote name="James 1" value="2" />
     </List>
   )
--- src/components/Vote/index.jsx
+++ src/components/Vote/index.jsx
@@ -5,8 +5,44 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import Zoom from '@mui/material/Zoom';
 
-const Vote = ({ name, value }) => {
+const Points = ({ value, isRevealed }) => {
+  if (!isRevealed) {
+    if (value != null) {
+      return (
+        <Zoom in>
+          <CheckCircleOutlineIcon color="success" />
+        </Zoom>
+      )
+    } else {
+      return (
+        <Zoom in>
+          <HourglassBottomIcon color="warning" />
+        </Zoom>
+      )
+    }
+  } else {
+    if (value != null) {
+      return (
+        <Zoom in>
+          <Typography color="primary">{ value }</Typography>
+        </Zoom>
+      )
+    } else {
+      return (
+        <Zoom in>
+          <Typography>--</Typography>
+        </Zoom>
+      )
+    }
+  }
+}
+
+const Vote = ({ name, value, isRevealed }) => {
   return (
     <ListItem>
       <ListItemAvatar sx={{ width: 4, minWidth: 0, marginRight: 2 }}>
@@ -14,7 +50,7 @@ const Vote = ({ name, value }) => {
       </ListItemAvatar>
       <ListItemText>{ name }</ListItemText>
       <ListItemSecondaryAction>
-        <ListItemText edge="end">{ value }</ListItemText>
+        <ListItemText edge="end"><Points value={value} isRevealed={isRevealed} /></ListItemText>
       </ListItemSecondaryAction>
     </ListItem>
   )
