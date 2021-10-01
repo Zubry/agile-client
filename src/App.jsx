4a38fd35cd008695abbe27f43f7529d417de3bba
zubry <zubryjs@gmail.com>
2021-10-01 17:21:10 -0400

    Add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react"
 import List from '@mui/material/List'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 import Vote from './components/Vote'
 
 function App() {
   return (
-    <List>
-      <Vote name="James" value="0.5" isRevealed/>
-      <Vote name="James 1" isRevealed />
-      <Vote name="James 1" />
-      <Vote name="James 1" value="2" />
-    </List>
+    <Card>
+      <CardContent>
+        <List>
+          <Vote name="James" value="0.5" isRevealed/>
+          <Vote name="James 1" isRevealed />
+          <Vote name="James 1" />
+          <Vote name="James 1" value="2" />
+        </List>
+      </CardContent>
+    </Card>
   )
 }
 
