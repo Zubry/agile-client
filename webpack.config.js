0bb85435c2755bcc71d392ae5606da4da15c9e41
zubry <zubryjs@gmail.com>
2021-10-01 16:36:29 -0400

    More setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /nodeModules/,
         use: {
           loader: 'babel-loader'
         }
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
   },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
@@ -24,5 +37,18 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   }
-};
+}
